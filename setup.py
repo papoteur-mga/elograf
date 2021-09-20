4187cda7fca4a6990a206a77d40692bef705f948
Papoteur <papoteur@mageia.org>
2021-09-20 07:20:49 +0200

    Fix managing translations in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os, glob
 
 class BuildQm(build):
   os.system('pylupdate5 elograf.pro')
-  for ts in glob.glob('translations/*.ts'):
+  for ts in glob.glob('eloGraf/translations/*.ts'):
     os.system('lrelease {0} -qm {1}'.format(ts, (ts[:-2]+'qm')))
 
 data_files = [("share/icons/hicolor/scalable/apps/", ["icons/elograf/scalable/micro.svg","icons/elograf/scalable/nomicro.svg"]),
