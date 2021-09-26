48f237b6e70e8d28f801e8be3deb50b5f3d9d070
Papoteur <papoteur@mageia.org>
2021-09-26 21:23:33 +0200

    0.1.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ data_files = [
 
 setup(
     name="elograf",
-    version="0.1.6",
+    version="0.1.7",
     packages=["eloGraf"],
     scripts=["elograf"],
     package_data={"": ["translations/*.qm"]},
