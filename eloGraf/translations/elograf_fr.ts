61827b4da83035c46008e3f8328928880a704c64
Papoteur <papoteur@mageia.org>
2021-10-18 08:37:21 +0200

    Update translatioin in French

--- eloGraf/translations/elograf_fr.ts
+++ eloGraf/translations/elograf_fr.ts
@@ -112,47 +112,47 @@
         <translation type="obsolete">Commande préliminaire :</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="147"/>
+        <location filename="../elograf.py" line="156"/>
         <source>Language</source>
         <translation>Langue</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="147"/>
+        <location filename="../elograf.py" line="156"/>
         <source>Name</source>
         <translation>Nom</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="147"/>
+        <location filename="../elograf.py" line="156"/>
         <source>Description</source>
         <translation>Description</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="147"/>
+        <location filename="../elograf.py" line="156"/>
         <source>Size</source>
         <translation>Taille</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="147"/>
+        <location filename="../elograf.py" line="156"/>
         <source>License</source>
         <translation>Licence</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="169"/>
+        <location filename="../elograf.py" line="178"/>
         <source>Not provided</source>
         <translation>Non précisé</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="139"/>
+        <location filename="../elograf.py" line="148"/>
         <source>Use custom model location</source>
         <translation>Utiliser un emplacement personnel de modèle</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="140"/>
+        <location filename="../elograf.py" line="149"/>
         <source>Select directory</source>
         <translation>Sélectionner un répertoire</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="258"/>
+        <location filename="../elograf.py" line="272"/>
         <source>Select the model path</source>
         <translation>Sélectionner le chemin du modèle</translation>
     </message>
@@ -162,30 +162,35 @@
         <translation type="obsolete">Commande postliminaire :</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="192"/>
+        <location filename="../elograf.py" line="201"/>
         <source>Advanced</source>
         <translation>Avancé</translation>
+    </message>
+    <message>
+        <location filename="../elograf.py" line="307"/>
+        <source>Default</source>
+        <translation>Par défaut</translation>
     </message>
 </context>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../advanced.ui" line="14"/>
+        <location filename="../advanced.ui" line="20"/>
         <source>Advanced parameters</source>
         <translation>Paramètres avancés</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="43"/>
+        <location filename="../advanced.ui" line="142"/>
         <source>Add option to add on the comamnd line of the dictation tool</source>
         <translation>Ajouter une option à mettre sur la ligne de commande de l&apos;outil de dictée</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="46"/>
+        <location filename="../advanced.ui" line="145"/>
         <source>Free option</source>
         <translation>Option libre</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="59"/>
+        <location filename="../advanced.ui" line="179"/>
         <source>Time out recording when no speech is processed for the time in seconds.
 This can be used to avoid having to explicitly exit
 (zero disables)</source>
@@ -194,12 +199,12 @@ Ceci peut être utilisé pour éviter une sortie explicite
 (une valeur à 0 désactive)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="64"/>
+        <location filename="../advanced.ui" line="184"/>
         <source>Timeout (s)</source>
         <translation>Délai d&apos;arrêt (s)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="71"/>
+        <location filename="../advanced.ui" line="242"/>
         <source>Time to idle between processing audio from the recording.
 Setting to zero is the most responsive at the cost of high CPU usage.
 The default value is 0.1 (processing 10 times a second),
@@ -210,98 +215,98 @@ La valeur par défaut est 0,1s (10 traitements/s),
 ce qui est relativement réactif en pratique.</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="77"/>
+        <location filename="../advanced.ui" line="248"/>
         <source>Idle time (ms)</source>
         <translation>Délai d&apos;attente (ms)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="84"/>
+        <location filename="../advanced.ui" line="46"/>
         <source>Use a comma separators for numbers</source>
         <translation>Utilise la virgule comme séparateur de nombres</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="87"/>
+        <location filename="../advanced.ui" line="49"/>
         <source>Use separator for numbers</source>
         <translation>Utilise un séparateur de nombres</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="114"/>
+        <location filename="../advanced.ui" line="121"/>
         <source>The time-out in seconds for detecting the state of dictation from the previous recording,
 this can be useful so punctuation it is added before entering the dictation (zero disables)</source>
         <translation>le délai en secondes pour détecter l&apos;état de la dictée depuis les paroles précédentes,
 ceci peut être utile pour l&apos;ajout de la ponctuation avant de dicter (une valeur 0 désactive)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="118"/>
+        <location filename="../advanced.ui" line="125"/>
         <source>Punctuate from previous timeout (s)</source>
         <translation>Ponctuer après le délai (s)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="125"/>
+        <location filename="../advanced.ui" line="97"/>
         <source>The name of the pulse-audio device to use for recording. 
 See the output of &quot;pactl list sources&quot; to find device names (using the identifier following &quot;Name:&quot;)</source>
         <translation>Le nom de l&apos;interface audio pulseaudio à utiliser pour l&apos;entrée son.
 Voir la sortie de &quot;pactl list sources&quot; pour trouver les noms (en utilisant l&apos;identiant après &quot;Nom&quot;)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="129"/>
+        <location filename="../advanced.ui" line="101"/>
         <source>Pulse device name</source>
-        <translation>Nome de l&apos;interafce pulse</translation>
+        <translation>Nom de l&apos;interface pulse</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="136"/>
+        <location filename="../advanced.ui" line="87"/>
         <source>Convert numbers into digits instead of using whole words</source>
         <translation>Convertir les nombres en chiffres au lieu d&apos;écrire en mots</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="139"/>
+        <location filename="../advanced.ui" line="90"/>
         <source>Numbers as digits</source>
         <translation>Nombres écrits en chiffres</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="149"/>
+        <location filename="../advanced.ui" line="132"/>
         <source>Command to execute before launching the dictation</source>
         <translation>Commende à exécuter avant de lancer la dictée</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="152"/>
+        <location filename="../advanced.ui" line="135"/>
         <source>Precommand</source>
         <translation>Commande préliminaire</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="159"/>
+        <location filename="../advanced.ui" line="28"/>
         <source>Capitalize the first character.
 This is also used to add either a comma or a full stop when dictation is performed according to previous delay</source>
         <translation>Mettre le premier caractère en majuscule.
 Ceci est aussi utilisé pour un soit mettre une virgule, soit mettre en arrêt lorsque la dictée est faite après le délai précédent</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="163"/>
+        <location filename="../advanced.ui" line="32"/>
         <source>Full sentence</source>
         <translation>Phrase complète</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="170"/>
+        <location filename="../advanced.ui" line="255"/>
         <source>The sample rate to use for recording (in Hz). Defaults to 44100</source>
         <translation>Le taux d&apos;échantillonnage à utiliser pour l&apos;enregistrement (en Hz). Par défaut à 44100</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="173"/>
+        <location filename="../advanced.ui" line="258"/>
         <source>Sample rate (Hz)</source>
         <translation>Taux d&apos;échantillonnage (Hz)</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="187"/>
+        <location filename="../advanced.ui" line="225"/>
         <source>Command to execute after the dictation is stopped</source>
         <translation>Commande à exécuter à la fin de la dictée</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="190"/>
+        <location filename="../advanced.ui" line="228"/>
         <source>Postcommand</source>
         <translation>Commande post-liminaire</translation>
     </message>
     <message>
-        <location filename="../advanced.ui" line="248"/>
+        <location filename="../advanced.ui" line="206"/>
         <source>TextLabel</source>
         <translation></translation>
     </message>
@@ -309,12 +314,12 @@ Ceci est aussi utilisé pour un soit mettre une virgule, soit mettre en arrêt l
 <context>
     <name>SystemTrayIcon</name>
     <message>
-        <location filename="../elograf.py" line="316"/>
+        <location filename="../elograf.py" line="345"/>
         <source>Exit</source>
         <translation>Quitter</translation>
     </message>
     <message>
-        <location filename="../elograf.py" line="317"/>
+        <location filename="../elograf.py" line="346"/>
         <source>Configuration</source>
         <translation>Configuration</translation>
     </message>
