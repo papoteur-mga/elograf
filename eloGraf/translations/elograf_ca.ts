d33853afe3c4430df3f868d3414561e593be7942
Yuri Chornoivan <yurchor@ukr.net>
2024-01-17 15:14:14 +0200

    Add Catalan translation

---
+++ eloGraf/translations/elograf_ca.ts
@@ -0,0 +1,431 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="1.1" language="ca">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="languages.py" line="4"/>
+        <source>English</source>
+        <translation>Anglès</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="5"/>
+        <source>Indian English</source>
+        <translation>Anglès indi</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="6"/>
+        <source>Chinese</source>
+        <translation>Xinès</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="7"/>
+        <source>Russian</source>
+        <translation>Rus</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="8"/>
+        <source>French</source>
+        <translation>Francès</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="9"/>
+        <source>German</source>
+        <translation>Alemany</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="10"/>
+        <source>Spanish</source>
+        <translation>Castellà</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="11"/>
+        <source>Portuguese/Brazilian Portuguese</source>
+        <translation>portuguès / portuguès brasiler</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="12"/>
+        <source>Greek</source>
+        <translation>Grec</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="13"/>
+        <source>Turkish</source>
+        <translation>Turc</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="14"/>
+        <source>Vietnamese</source>
+        <translation>Vietnamita</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="15"/>
+        <source>Italian</source>
+        <translation>Italià</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="16"/>
+        <source>Dutch</source>
+        <translation>Holandès</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="17"/>
+        <source>Catalan</source>
+        <translation>Català</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="18"/>
+        <source>Arabic</source>
+        <translation>Àrab</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="19"/>
+        <source>Farsi</source>
+        <translation>Farsi</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="20"/>
+        <source>Filipino</source>
+        <translation>Filipí</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="21"/>
+        <source>Ukrainian</source>
+        <translation>Ucraïnès</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="22"/>
+        <source>Kazakh</source>
+        <translation>Kazakh</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="23"/>
+        <source>Speaker identification model</source>
+        <translation>Model d&apos;identificació del parlant</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigPopup</name>
+    <message>
+        <location filename="elograf.py" line="540"/>
+        <source>Active direct click on icon</source>
+        <translation>Clic directe actiu a la icona</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="543"/>
+        <source>Import remote model</source>
+        <translation>Importa el model remot</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="545"/>
+        <source>Import local model</source>
+        <translation>Importa el model local</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="547"/>
+        <source>Advanced</source>
+        <translation>Avançat</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="682"/>
+        <source>We will download the list of models from {}.
+Do you agree?</source>
+        <translation>Baixarem la llista de models des de {}.
+Hi esteu d&apos;acord?</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="758"/>
+        <source>Default</source>
+        <translation>Per defecte</translation>
+    </message>
+</context>
+<context>
+    <name>CustomUI</name>
+    <message>
+        <location filename="elograf.py" line="294"/>
+        <source>Select the model path</source>
+        <translation>Seleccioneu la ruta del model</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="advanced.ui" line="20"/>
+        <source>Advanced parameters</source>
+        <translation>Paràmetres avançats</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="28"/>
+        <source>Capitalize the first character.
+This is also used to add either a comma or a full stop when dictation is performed according to previous delay</source>
+        <translation>Posa en majúscula el primer caràcter.
+També s&apos;usa per afegir una coma o un punt quan es fa el dictat segons el retard anterior.</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="32"/>
+        <source>Full sentence</source>
+        <translation>Frase completa</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="46"/>
+        <source>Use a comma separators for numbers</source>
+        <translation>Usa separadors de comes per als números</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="49"/>
+        <source>Use separator for numbers</source>
+        <translation>Usa el separador per als números</translation>
+    </message>
+    <message>
+        <location filename="confirm.ui" line="41"/>
+        <source>TextLabel</source>
+        <translation>Text de l&apos;etiqueta</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="87"/>
+        <source>Convert numbers into digits instead of using whole words</source>
+        <translation>Converteix números en dígits en lloc de paraules senceres</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="90"/>
+        <source>Numbers as digits</source>
+        <translation>Números com a dígits</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="97"/>
+        <source>The name of the pulse-audio device to use for recording. 
+See the output of &quot;pactl list sources&quot; to find device names (using the identifier following &quot;Name:&quot;)</source>
+        <translation>El nom del dispositiu d&apos;àudio que s&apos;usarà per a la gravació.
+Consulteu la sortida de &quot;fonts de la llista pactl&quot; per trobar noms de dispositius (amb l&apos;identificador següent a &quot;Nom:&quot;)</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="101"/>
+        <source>Pulse device name</source>
+        <translation>Nom del dispositiu de Pulse</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="121"/>
+        <source>The time-out in seconds for detecting the state of dictation from the previous recording,
+this can be useful so punctuation it is added before entering the dictation (zero disables)</source>
+        <translation>El temps d&apos;espera en segons per detectar l&apos;estat del dictat de la gravació anterior.
+Això pot ser útil perquè la puntuació s&apos;afegeix abans d&apos;entrar al dictat. (Un zero ho desactiva).</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="125"/>
+        <source>Punctuate from previous timeout (s)</source>
+        <translation>Puntua des del temps d&apos;espera anterior (s)</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="132"/>
+        <source>Command to execute before launching the dictation</source>
+        <translation>Ordre per executar abans d&apos;obrir el dictat</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="135"/>
+        <source>Precommand</source>
+        <translation>Ordre prèvia</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="142"/>
+        <source>Add option to add on the comamnd line of the dictation tool</source>
+        <translation>Afegeix una opció per afegir a la línia d&apos;ordres de l&apos;eina de dictat</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="145"/>
+        <source>Free option</source>
+        <translation>Opció lliure</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="179"/>
+        <source>Time out recording when no speech is processed for the time in seconds.
+This can be used to avoid having to explicitly exit
+(zero disables)</source>
+        <translation>Temps d&apos;espera de la gravació quan no es processa cap discurs durant el temps en segons.
+Això es pot usar per evitar haver de sortir explícitament.
+(Un zero ho desactiva.)</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="184"/>
+        <source>Timeout (s)</source>
+        <translation>Temps d&apos;espera (s)</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="225"/>
+        <source>Command to execute after the dictation is stopped</source>
+        <translation>Ordre per executar després d&apos;aturar el dictat</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="228"/>
+        <source>Postcommand</source>
+        <translation>Ordre posterior</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="242"/>
+        <source>Time to idle between processing audio from the recording.
+Setting to zero is the most responsive at the cost of high CPU usage.
+The default value is 0.1 (processing 10 times a second),
+which is quite responsive in practice</source>
+        <translation>Temps d&apos;inactivitat entre el processament d&apos;àudio de la gravació.
+Establir-lo a zero és el més sensible a costa d&apos;un ús elevat de la CPU.
+El valor per defecte és 0,1 (processament 10 vegades per segon),
+que és bastant sensible a la pràctica.</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="248"/>
+        <source>Idle time (ms)</source>
+        <translation>Temps d&apos;inactivitat (ms)</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="255"/>
+        <source>The sample rate to use for recording (in Hz). Defaults to 44100</source>
+        <translation>La freqüència de mostratge que s&apos;usarà per a la gravació (en Hz). El valor predeterminat és 44100.</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="258"/>
+        <source>Sample rate (Hz)</source>
+        <translation>Freqüència de mostratge (Hz)</translation>
+    </message>
+    <message>
+        <location filename="confirm.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>Diàleg</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="14"/>
+        <source>Select a custom model</source>
+        <translation>Seleccioneu un model personalitzat</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="34"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select a custom model&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Seleccioneu un model personalitzat&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="46"/>
+        <source>Path</source>
+        <translation>Camí</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="53"/>
+        <source>Select the path of your model</source>
+        <translation>Seleccioneu el camí del vostre model</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="60"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Give a name to your model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Poseu un nom al model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="63"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="73"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indicate a class to your model like small, medium or big&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indiqueu una classe per al model, com ara petit, mitjà o gros&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="76"/>
+        <source>Class</source>
+        <translation>Classe</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="86"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Give a version number to your model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Establiu un número de versió per al model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="89"/>
+        <source>Version</source>
+        <translation>Versió</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="99"/>
+        <source>Size</source>
+        <translation>Mida</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="122"/>
+        <source>Language</source>
+        <translation>Llengua</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadPopup</name>
+    <message>
+        <location filename="elograf.py" line="391"/>
+        <source>Import system wide</source>
+        <translation>Importa&apos;l a tot el sistema</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="393"/>
+        <source>Import in user space</source>
+        <translation>Importa&apos;l a l&apos;espai de l&apos;usuari</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="433"/>
+        <source>The application failed to save the model. Do you want to retry?</source>
+        <translation>L&apos;aplicació no ha pogut desar el model. Voleu tornar-ho a provar?</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="445"/>
+        <source>The model can&apos;t be saved. Check for space available or credentials for {}</source>
+        <translation>El model no es pot desar. Comproveu si hi ha espai disponible o credencials per a {}.</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="471"/>
+        <source>We will download the model {} of {} from {}. Do you agree?</source>
+        <translation>Baixarem el model {} de {} de {}. Hi esteu d&apos;acord?</translation>
+    </message>
+</context>
+<context>
+    <name>Models</name>
+    <message>
+        <location filename="elograf.py" line="92"/>
+        <source>Language</source>
+        <translation>Llengua</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="93"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="94"/>
+        <source>Version</source>
+        <translation>Versió</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="95"/>
+        <source>Size</source>
+        <translation>Mida</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="96"/>
+        <source>Class</source>
+        <translation>Classe</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTrayIcon</name>
+    <message>
+        <location filename="elograf.py" line="802"/>
+        <source>Start dictation</source>
+        <translation>Comença el dictat</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="803"/>
+        <source>Stop dictation</source>
+        <translation>Interromp el dictat</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="806"/>
+        <source>Configuration</source>
+        <translation>Configuració</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="807"/>
+        <source>Exit</source>
+        <translation>Surt</translation>
+    </message>
+</context>
+</TS>
