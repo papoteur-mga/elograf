5b63791b85ffac7bad689444332f81084feb6ec9
Yuri Chornoivan <yurchor@ukr.net>
2024-07-06 19:35:09 +0300

    Add Portuguese translations from Tx

---
+++ eloGraf/translations/elograf_pt.ts
@@ -0,0 +1,422 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="1.1" language="pt">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="languages.py" line="4"/>
+        <source>English</source>
+        <translation>Inglês</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="5"/>
+        <source>Indian English</source>
+        <translation>Inglês (Índia)</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="6"/>
+        <source>Chinese</source>
+        <translation>Chinês</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="7"/>
+        <source>Russian</source>
+        <translation>Russo</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="8"/>
+        <source>French</source>
+        <translation>Francês</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="9"/>
+        <source>German</source>
+        <translation>Alemão</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="10"/>
+        <source>Spanish</source>
+        <translation>Espanhol</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="11"/>
+        <source>Portuguese/Brazilian Portuguese</source>
+        <translation>Português (Portugal)/Português (Brasil)</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="12"/>
+        <source>Greek</source>
+        <translation>Grego</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="13"/>
+        <source>Turkish</source>
+        <translation>Turco</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="14"/>
+        <source>Vietnamese</source>
+        <translation>Vietnamita</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="15"/>
+        <source>Italian</source>
+        <translation>Italiano</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="16"/>
+        <source>Dutch</source>
+        <translation>Holandês</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="17"/>
+        <source>Catalan</source>
+        <translation>Catalão</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="18"/>
+        <source>Arabic</source>
+        <translation>Árabe</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="19"/>
+        <source>Farsi</source>
+        <translation>Farsi</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="20"/>
+        <source>Filipino</source>
+        <translation>Filipino</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="21"/>
+        <source>Ukrainian</source>
+        <translation>Ucraniano</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="22"/>
+        <source>Kazakh</source>
+        <translation>Cazaque</translation>
+    </message>
+    <message>
+        <location filename="languages.py" line="23"/>
+        <source>Speaker identification model</source>
+        <translation>Modelo de identificação do falante</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigPopup</name>
+    <message>
+        <location filename="elograf.py" line="540"/>
+        <source>Active direct click on icon</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="543"/>
+        <source>Import remote model</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="545"/>
+        <source>Import local model</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="547"/>
+        <source>Advanced</source>
+        <translation>Avançado</translation>
+    </message>
+    <message>
+        <location filename="elograf.py" line="682"/>
+        <source>We will download the list of models from {}.
+Do you agree?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="758"/>
+        <source>Default</source>
+        <translation>Predefinição</translation>
+    </message>
+</context>
+<context>
+    <name>CustomUI</name>
+    <message>
+        <location filename="elograf.py" line="294"/>
+        <source>Select the model path</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="advanced.ui" line="20"/>
+        <source>Advanced parameters</source>
+        <translation>Parâmetros avançados</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="28"/>
+        <source>Capitalize the first character.
+This is also used to add either a comma or a full stop when dictation is performed according to previous delay</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="32"/>
+        <source>Full sentence</source>
+        <translation>Frase completa</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="46"/>
+        <source>Use a comma separators for numbers</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="49"/>
+        <source>Use separator for numbers</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="confirm.ui" line="41"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="87"/>
+        <source>Convert numbers into digits instead of using whole words</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="90"/>
+        <source>Numbers as digits</source>
+        <translation>Números como dígitos</translation>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="97"/>
+        <source>The name of the pulse-audio device to use for recording. 
+See the output of &quot;pactl list sources&quot; to find device names (using the identifier following &quot;Name:&quot;)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="101"/>
+        <source>Pulse device name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="121"/>
+        <source>The time-out in seconds for detecting the state of dictation from the previous recording,
+this can be useful so punctuation it is added before entering the dictation (zero disables)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="125"/>
+        <source>Punctuate from previous timeout (s)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="132"/>
+        <source>Command to execute before launching the dictation</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="135"/>
+        <source>Precommand</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="142"/>
+        <source>Add option to add on the comamnd line of the dictation tool</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="145"/>
+        <source>Free option</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="179"/>
+        <source>Time out recording when no speech is processed for the time in seconds.
+This can be used to avoid having to explicitly exit
+(zero disables)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="184"/>
+        <source>Timeout (s)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="225"/>
+        <source>Command to execute after the dictation is stopped</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="228"/>
+        <source>Postcommand</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="242"/>
+        <source>Time to idle between processing audio from the recording.
+Setting to zero is the most responsive at the cost of high CPU usage.
+The default value is 0.1 (processing 10 times a second),
+which is quite responsive in practice</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="248"/>
+        <source>Idle time (ms)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="255"/>
+        <source>The sample rate to use for recording (in Hz). Defaults to 44100</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="advanced.ui" line="258"/>
+        <source>Sample rate (Hz)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="confirm.ui" line="20"/>
+        <source>Dialog</source>
+        <translation>Diálogo</translation>
+    </message>
+    <message>
+        <location filename="custom.ui" line="14"/>
+        <source>Select a custom model</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="34"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select a custom model&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="46"/>
+        <source>Path</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="53"/>
+        <source>Select the path of your model</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="60"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Give a name to your model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="63"/>
+        <source>Name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="73"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Indicate a class to your model like small, medium or big&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="76"/>
+        <source>Class</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="86"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Give a version number to your model&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="89"/>
+        <source>Version</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="99"/>
+        <source>Size</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="custom.ui" line="122"/>
+        <source>Language</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>DownloadPopup</name>
+    <message>
+        <location filename="elograf.py" line="391"/>
+        <source>Import system wide</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="393"/>
+        <source>Import in user space</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="433"/>
+        <source>The application failed to save the model. Do you want to retry?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="445"/>
+        <source>The model can&apos;t be saved. Check for space available or credentials for {}</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="471"/>
+        <source>We will download the model {} of {} from {}. Do you agree?</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Models</name>
+    <message>
+        <location filename="elograf.py" line="92"/>
+        <source>Language</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="93"/>
+        <source>Name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="94"/>
+        <source>Version</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="95"/>
+        <source>Size</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="96"/>
+        <source>Class</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SystemTrayIcon</name>
+    <message>
+        <location filename="elograf.py" line="802"/>
+        <source>Start dictation</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="803"/>
+        <source>Stop dictation</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="806"/>
+        <source>Configuration</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="elograf.py" line="807"/>
+        <source>Exit</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
